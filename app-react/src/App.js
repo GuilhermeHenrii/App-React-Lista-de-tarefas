cc97d9de77fb8f4cdf1b97415fa7fe75597e599f
Guilherme Henrique <guilhermesilva50239@gmail.com>
2023-10-14 16:00:32 -0300

    'Configs'

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+// sempre que formos usar o jsx, devemos primeiro importar o react
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Word
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import './App.css' // esse aquivo css irá estilizar o componente
 
-export default App;
+// componente react
+export default function App() {
+  return <h1>Hello Word!</h1>
+};
